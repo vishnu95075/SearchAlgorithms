a43867ac4f5b431f2d702f22234e0548aaf39f16
CodeWagon <94778936+CodeWagon@users.noreply.github.com>
2021-11-24 21:16:14 +0530

    Create LinearSearch.cpp

---
+++ LinearSearch.cpp
@@ -0,0 +1,36 @@
+
+// C++ code for linear search
+// Compare every single element in the array 
+// with the search key
+#include <iostream>
+using namespace std;
+ 
+int search(int arr[], int n, int x)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
+}
+ 
+// Driver code
+int main(void)
+{
+    // arr - array of element
+    int arr[] = { 2, 3, 4, 10, 40 };
+  
+    // x - search key
+    int x = 10;
+  
+    // n - size of array (found by dividing size of 
+    // entire array by size of single element from the array
+    int n = sizeof(arr) / sizeof(arr[0]);
+   
+    // Function call
+    int result = search(arr, n, x);
+    (result == -1)
+        ? cout << "Element is not in the array :-("
+        : cout << "Element found at position :" << result;
+    return 0;
+}
